f4530566b3bbafc923cd5f4e7fd7316433ae7027
mdsam19 <mdshakib110199@gmail.com>
2023-08-09 16:23:28 +0000

    final

---
+++ lua/colorscheme.lua
@@ -0,0 +1,5 @@
+--tokyonight colorscheme
+
+vim.cmd([[colorscheme tokyonight]])
+vim.g.tokyonight_style = "storm"
+vim.g.tokyonight_italic_functions = false
---
+++ lua/lsp/init.lua
@@ -0,0 +1,33 @@
+--Enable (broadcasting) snippet capability for completion
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+--Language servers
+require("lspconfig").pyright.setup({ capabilities = capabilities })
+require("lspconfig").tsserver.setup({ capabilities = capabilities })
+require("lspconfig").svelte.setup({ capabilities = capabilities })
+require("lspconfig").tailwindcss.setup({ capabilities = capabilities })
+require("lspconfig").prismals.setup({})
+require("lspconfig").cssls.setup({ capabilities = capabilities })
+require("lspconfig").lua_ls.setup({
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
+})
+require("lspconfig").emmet_ls.setup({
+	capabilities = capabilities,
+	filetypes = { "html", "typescriptreact", "javascriptreact" },
+})
